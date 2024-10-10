9aa876f943e40f364b2e4f58a0f3e1fe252f2fb8
Rickson Oliveira <88171582+Rickson298@users.noreply.github.com>
2024-10-10 09:36:36 -0300

    counter stress test

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,206 @@
+import { useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <ScrollView style={{ margin: 100 }}>
+      <Button
+        title="Incrementar"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+      <Text style={{ fontSize: 24, fontWeight: 600 }}>{counter}</Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
